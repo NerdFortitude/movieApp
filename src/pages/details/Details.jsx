fe36839c4b5418c274acb6afc1dc50be2c77fc59
Nishant Rai <Nishant Rai>
2023-10-02 10:48:40 +0530

    details page completed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,7 +4,9 @@ import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
-
+import VideosSection from './videoSection/VideoSection';
+import Similar from './carousels/Similar';
+import Recommendation from './carousels/Recommendations';
 
 
 function Details() {
@@ -12,13 +14,16 @@ function Details() {
   const {mediaType, id} = useParams();
   const {data,loading} = useFetch(`/${mediaType}/${id}/videos`);
   const {data : credits, loading: creditLoading} = useFetch(`/${mediaType}/${id}/credits`)
-
+  
  
   return (
     <div>
         <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
 
         <Cast data={credits?.cast} loading={creditLoading}></Cast>
+        <VideosSection data={data} loading={loading}/>
+        <Similar mediaType={mediaType} id={id}></Similar>
+        <Recommendation mediaType={mediaType} id={id}></Recommendation>
     </div>
   )
 }
